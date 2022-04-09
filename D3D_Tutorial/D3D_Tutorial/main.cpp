d5d41f53c5d917315b1706c9b77c8be274426ca1
dushengsheng <dushengsheng.51@bytedance.com>
2022-04-09 23:20:04 +0800

    WinMain 程序可以运行

---
+++ D3D_Tutorial/D3D_Tutorial/main.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,		// 应用程序句柄
+	HINSTANCE hPrevInstance,	// 上一个应用程序的句柄，目前已弃用 一直为 NULL
+	LPSTR     lpCmdLine,		// 传入的命令行
+	int       nShowCmd			// 设置窗口的显示方式
+)
+{
+
+
+	return 0;
+}
+
